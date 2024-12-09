7f7d52f7935dac792a783774d661d82498e1d297
Ivan Kisselev <ivankisselev7@gmail.com>
2024-12-08 19:59:59 -0500

    made file to easily compare model accuracies

---
+++ src/eval_acc_comparison.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+
+# Aggregated Data Feature Model
+testing_file = "output/aggregated_data_testing_normalized"
+model_file = "trained_model_aggregated_data.keras"
+columns = ["label"] + [f"data{i+1}" for i in range(30)]
+data_testing = pd.read_csv(testing_file, sep=" ", header=None, names=columns)
+
+values_testing = data_testing.iloc[:, 1:].values
+labels_testing = data_testing.iloc[:, 0].values
+
+label_encoder = LabelEncoder()
+labels_testing = label_encoder.fit_transform(labels_testing)
+labels_testing = to_categorical(labels_testing)
+
+values_testing = np.array(values_testing)
+labels_testing = np.array(labels_testing)
+
+if os.path.exists(model_file):
+    model = load_model(model_file)
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+else:
+    print(f"model {model_file} not found")
+    exit()
+
+loss, accuracy = model.evaluate(values_testing, labels_testing, verbose=0)
+print("\nAggregated Data Extraction Model Results:")
+print("(Features are data sent per 0.1 second interval within the first 3 seconds)")
+print(f"Validation Loss: {loss:.4f}")
+print(f"Validation Accuracy: {accuracy:.4f}")
+
+# Summary Stats Feature Model
+testing_file = "output/summary_stats_testing_normalized"
+model_file = "trained_model_summary_stats.keras"
+columns = ["label", "numPackets", "total_data_sent", "stdev_arrival_times", "avg_inter_arrival_time", "median_arrival_time"]
+data_testing = pd.read_csv(testing_file, sep=" ", header=None, names=columns)
+
+values_testing = data_testing.iloc[:, 1:].values
+labels_testing = data_testing.iloc[:, 0].values
+
+label_encoder = LabelEncoder()
+labels_testing = label_encoder.fit_transform(labels_testing)
+labels_testing = to_categorical(labels_testing)
+
+values_testing = np.array(values_testing)
+labels_testing = np.array(labels_testing)
+
+if os.path.exists(model_file):
+    model = load_model(model_file)
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+else:
+    print(f"model {model_file} not found")
+    exit()
+
+loss, accuracy = model.evaluate(values_testing, labels_testing, verbose=0)
+print("\nSummary Stats Extraction Model Results:")
+print("(Features are numPackets, total_data_sent, stdev_arrival_times, avg_inter_arrival_time, and median_arrival_time)")
+print(f"Validation Loss: {loss:.4f}")
+print(f"Validation Accuracy: {accuracy:.4f}")
